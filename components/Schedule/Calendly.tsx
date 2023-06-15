7725eb68910ab2f3c1425110c38e80aa2b6859fe
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-06-15 13:28:03 -0300

    add confirmation modal to schedule page

--- components/Schedule/Calendly.tsx
+++ components/Schedule/Calendly.tsx
@@ -1,7 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import style from "./calendly.module.css";
+import Link from "next/link";
+import confirmed from "./confirmed-svgrepo-com.svg";
 
 const Calendly = () => {
+  const [isConfirmed, setIsConfirmed] = useState(false);
+  const router = useRouter();
+
   function isCalendlyEvent(e: any) {
     return (
       e.origin === "https://calendly.com" &&
@@ -19,6 +26,12 @@ const Calendly = () => {
 
           /* Example to get the payload of the event */
           console.log("Event details:", e.data.payload);
+
+          if (e.data.event == "calendly.event_scheduled") {
+            console.log("confirmado!");
+            // return router.push("/confirmed");
+            setIsConfirmed(true);
+          }
         }
       });
     }
@@ -36,6 +49,21 @@ const Calendly = () => {
         src="https://assets.calendly.com/assets/external/widget.js"
         async
       ></Script>
+      {isConfirmed && (
+        <div className={style.confirmed}>
+          <img src={confirmed.src} alt="" />
+          <h1>Sua sessão foi agendada com sucesso!</h1>{" "}
+          <h3> Um e-mail foi enviado com os detalhes do seu agendamento</h3>
+          <small>
+            {" "}
+            Verifique sua pasta de spam se o e-mail não chegar em alguns minutos
+          </small>
+          <Link className={style.button} href="/home">
+            {" "}
+            Voltar para o site
+          </Link>
+        </div>
+      )}
     </>
   );
 };
---
+++ pages/confirmed.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const confirmed = () => {
+  return <div>Confirmado!</div>;
+};
+
+export default confirmed;
